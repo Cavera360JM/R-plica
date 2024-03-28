52bfae898b51748914b2a24480e093993e43e867
João Miguel <97974646+Cavera360JM@users.noreply.github.com>
2024-03-28 17:04:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Adicionar eventos de hover para mudar a cor do cursor
-    var hoverElements = document.querySelectorAll('.hover-change');
-    hoverElements.forEach(function(element) {
-        element.addEventListener('mouseenter', function() {
-            document.body.style.cursor = 'url(), auto';
-        });
-        element.addEventListener('mouseleave', function() {
-            document.body.style.cursor = 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-kjmBt61exlti_bEGYC_5uW_SYm7_wGu2HQ&usqp=CAU), auto';
-        });
-    });
-});
-
 
